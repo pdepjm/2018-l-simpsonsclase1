b2d841ea21830971e8f62acb93aa85383782dc3b
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-06-14 17:06:41 -0300

    agrego lo que hicimos en k2004 la segunda parte de la clase

--- simpsons.pl
+++ simpsons.pl
@@ -28,3 +28,32 @@ tieneSombrero(elRabinoKrustovsky).
 
 convierteEnGrilloACualquiera(selma).
 convierteEnGrilloACualquiera(45).
+
+
+hijxDe(homero,abe).
+hijxDe(herbert,abe).
+hijxDe(bart,homero).
+hijxDe(lisa,homero).
+hijxDe(maggie,homero).
+hijxDe(lisa,marge).
+hijxDe(maggie,marge).
+hijxDe(bart,marge).
+hijxDe(rod,ned).
+hijxDe(tod,ned).
+hijxDe(milhouse,kirk).
+
+hermanxDe(Uno,Otro):-
+    hijxDe(Uno,Padre),
+    hijxDe(Otro,Padre),
+    Otro \= Uno.
+
+hijxUnicx(Alguien):-
+    hijxDe(Alguien,_), 
+    not(hermanxDe(Alguien,_)).
+
+% La razón para que exista la primera condición "hijxDe(Alguien,_),"
+% es, por un lado que si no la agregao, hijxUnicx(45) es verdadero.
+%  Además, si no la agrego, hijxUnicx NO es INVERSIBLE, es decir que
+% si yo pregunto hijxUnicx(Quien) no sabe darme ejemplos.
+% Un predicado es inversible cuando sabe darme ejemplos que lo hacen verdadero.
+% Y hay algunos predicados que no saben, como el \= .
